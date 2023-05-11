a145b0dbef98c3cd8929e850c67b8ab325d98937
R3TICULAR <98478018+R3TICULAR@users.noreply.github.com>
2023-05-11 17:49:19 -0400

    node modules and app.js implementation

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3002;
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, () => {
+    console.log(`App is UP on port ${PORT}`);
+});
