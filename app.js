22c605df18b6314b6d8cf9921cdf85c84ecbc9ce
R3TICULAR <98478018+R3TICULAR@users.noreply.github.com>
2023-05-11 18:34:37 -0400

    app.js modification to apply css styles

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const PORT = process.env.PORT || 3002;
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.listen(PORT, () => {
