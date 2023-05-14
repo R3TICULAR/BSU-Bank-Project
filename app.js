47df24bcb3aba54ce0fa61f1cc47de40475d1d22
R3TICULAR <98478018+R3TICULAR@users.noreply.github.com>
2023-05-13 21:28:40 -0400

    bg img

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3002;
 app.get("/", function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname + '/public')));
 
 
 app.listen(PORT, () => {
